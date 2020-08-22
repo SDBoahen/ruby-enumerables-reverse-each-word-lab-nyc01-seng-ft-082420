b3e4afdafdf0ba812bca38ff2a15a92aef32cd61
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-22 02:10:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,15 @@ def
   
   finesse_array = text_or_sting.split(" ")
   
-  rev_text = finesse_array.map { |each_string| each_string.reverse }
+  rev_text = finesse_array.collect { |each_string| each_string.reverse }
   rev_text.join(" ")
+  
+  
+  
+  
+  # ._.  . . . even though i like .map . . . they want (.collect) .-.
+  #  {...}
+  #  rev_text = finesse_array.map { |each_string| each_string.reverse }
   
 end 
 
